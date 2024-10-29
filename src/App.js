ca04346b220dfa8b1c6af5a472c00b4a68740535
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2024-10-30 01:05:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react'
 import Nav from './components/nav'
 import Hero from './components/hero'
 import './App.css'
+import Course from './components/Course'
+import Mentors from'./components/mentors'
+import Reviews from './components/reviews'
+import Features from './components/features'
+import Footer from './components/footer'
 function App() {
   return (
     <div>
       <Nav/>
       <Hero/>
+      <div className="per"><p>OUR COURSES</p></div>
+      <div class="products w-screen flex flex-wrap justify-between items-center px-[30px] py-[10px]"><Course/></div>
+      <Mentors/>
+      <Features/> 
+      <Reviews/>
+      <Footer/>
     </div>
-
   )
 }
 
