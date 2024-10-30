d738541dc386f5eb18e265d2f27b2f85a179f8e0
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2024-10-31 00:21:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     
       <Hero/>
       <div className="per"><p>OUR COURSES</p></div>
-      <div class="products w-screen flex flex-wrap justify-between items-center px-[30px] py-[10px]"><Course/></div>
+      <div class="products"><Course/></div>
       <Mentors/>
       <Features/> 
       <Reviews/>
