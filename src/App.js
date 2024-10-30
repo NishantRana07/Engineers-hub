e3f9a6926b56ccaed3ab0fdfbdcd6fcd669ed990
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2024-10-30 23:19:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/footer'
 function App() {
   return (
     <div>
-      <Nav/>
+    
       <Hero/>
       <div className="per"><p>OUR COURSES</p></div>
       <div class="products w-screen flex flex-wrap justify-between items-center px-[30px] py-[10px]"><Course/></div>
